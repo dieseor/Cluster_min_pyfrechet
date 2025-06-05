659ad8aa15c4eaea1b3c27c64ecedb1a923dc4bf
Diego <Diego@MacBook-Pro-2.local>
2025-06-05 21:56:23 +0200

    job

--- job_euc.sh
+++ job_euc.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-module load python/3.9.9
+module load cesga/2020 python/3.9.9
+export PYTHONNOUSERSITE=1
+source $STORE/pballs/bin/activate
 lscpu
 python simulations_euc/sklearn_main_parallel.py $block
 git add .
 git commit -m "Simulations Euclidean block $block"
 git push
 
-#for b in {1..4}
-#do
-#  sbatch --time=00:35:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="%j-euc-block$b.out" job_euc.sh
-#done
+# for b in {1..8}
+# do
+#   sbatch --time=01:20:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="%j-euc-block$b.out" job_euc.sh
+# done
 
-# sbatch --time=00:35:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=1 --output="%j-euc-block1.out" job_euc.sh
+# sbatch --time=01:20:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=1 --output="%j-euc-block1.out" job_euc.sh
