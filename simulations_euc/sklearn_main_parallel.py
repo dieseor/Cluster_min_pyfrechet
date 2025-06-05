609579e0c5f870c6fa53ac5196931bcc12476765
Diego <Diego@MacBook-Pro-2.local>
2025-06-05 13:21:50 +0200

    euc sims

--- simulations_euc/sklearn_main_parallel.py
+++ simulations_euc/sklearn_main_parallel.py
@@ -17,12 +17,12 @@ np.random.seed(1000)
 sign_level = np.array([0.01, 0.05, 0.1])
 betas = np.array([1, -1, 1])  # Define the true beta values
 
-data_dir = os.path.join(os.getcwd(), 'simulations_euc', 'pb_25')
+data_dir = os.path.join(os.getcwd(), 'simulations_euc', 'data')
 results_dir = os.path.join(os.getcwd(), 'simulations_euc', 'results')
 os.makedirs(results_dir, exist_ok=True)
 
 # By-blocks execution
-n_samples=len(os.listdir(os.path.join(os.getcwd(), 'simulations_euc/' 'pb_25')))
+n_samples=len(os.listdir(os.path.join(os.getcwd(), 'simulations_euc/' 'data')))
 current_block = int(sys.argv[1])
 
 # Define parameter grid for tuning
@@ -96,6 +96,7 @@ def task(file):
     n_predictors = X.shape[1]
 
     # Conformal data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
     # Measure time for tuning and fitting (prediction balls)
     start_time = time.time()
     # Perform hyperparameter tuning
@@ -107,13 +108,18 @@ def task(file):
     # Measure time for tuning and fitting (SC regions)
     start_time = time.time()
     # Perform hyperparameter tuning
+    conf_forest = tune_forest(X_train, y_train, param_grid) 
     # Radii 
+    test_preds = conf_forest.predict(X_test)
+    quantile = np.percentile(np.abs(y_test - test_preds), (1 - np.array([0.01, 0.05, 0.1])) * 100, method='inverted_cdf')
     end_time = time.time()
+    conf_time = end_time - start_time
 
 ############################################################################################################
     # TYPE I COVERAGE RESULTS
     n_estimations = 50
     pb_i_cov = np.zeros(shape = (n_estimations, 3))
+    conf_i_cov = np.zeros(shape = (n_estimations, 3))
 
     for estimation in range(n_estimations):
         # Randomly select rows from the dataframe
@@ -124,8 +130,10 @@ def task(file):
 
         # Predict the new observation
         pb_new_pred = pb_forest.predict(new_X)
+        conf_new_pred = conf_forest.predict(new_X)
 
         pb_i_cov[estimation, :] = (np.abs(pb_new_pred - new_y) <= oob_quantile)
+        conf_i_cov[estimation, :] = (np.abs(conf_new_pred - new_y) <= quantile)
             
 
 ############################################################################################################            
@@ -136,13 +144,16 @@ def task(file):
     new_X, new_y = simulate_data(sigma = true_sigma, X_design=new_X, betas = betas)
 
     pb_new_pred = pb_forest.predict(new_X)
+    conf_new_pred = conf_forest.predict(new_X)
 
     pb_ii_cov = np.sum(np.abs(pb_new_pred.reshape(-1,1) - new_y).reshape(-1,1) <= np.tile(oob_quantile, (MC, 1)), axis = 0) / MC
+    conf_ii_cov = np.sum(np.abs(conf_new_pred.reshape(-1,1) - new_y).reshape(-1,1) <= np.tile(quantile, (MC, 1)), axis = 0) / MC
 
 ############################################################################################################
     # TYPE III COVERAGE RESULTS
     q_25 = 2*np.sqrt(5)*(beta(2,2).ppf(.25)-1/2)
     pb_iii_cov = np.zeros(shape = (n_estimations, 3))
+    conf_iii_cov = np.zeros(shape = (n_estimations, 3))
 
     for estimation in range(n_estimations):
         # Randomly select rows from the dataframe
@@ -152,8 +163,10 @@ def task(file):
 
         # Predict the new observation
         pb_new_pred = pb_forest.predict(new_X)
+        conf_new_pred = conf_forest.predict(new_X)
 
         pb_iii_cov[estimation, :] = (np.abs(pb_new_pred - new_y) <= oob_quantile)
+        conf_iii_cov[estimation, :] = (np.abs(conf_new_pred - new_y) <= quantile)
 
 ############################################################################################################
     # TYPE IV COVERAGE RESULTS
@@ -162,8 +175,10 @@ def task(file):
     new_X, new_y = simulate_data(sigma = true_sigma, X_design=new_X, betas = betas)
 
     pb_new_pred = pb_forest.predict(new_X)
+    conf_new_pred = conf_forest.predict(new_X)
 
     pb_iv_cov = np.sum(np.abs(pb_new_pred.reshape(-1,1) - new_y).reshape(-1,1) <= np.tile(oob_quantile, (MC, 1)), axis = 0) / MC
+    conf_iv_cov = np.sum(np.abs(conf_new_pred.reshape(-1,1) - new_y).reshape(-1,1) <= np.tile(quantile, (MC, 1)), axis = 0) / MC
 
 ############################################################################################################
     # MSE
@@ -172,23 +187,32 @@ def task(file):
     X_test, y_test = simulate_data(sigma = true_sigma, X_design=new_X_design, betas = betas)
 
     pb_new_pred = pb_forest.predict(X_test)
+    conf_new_pred = conf_forest.predict(X_test)
 
     pb_mse = mse(pb_new_pred, y_test)
+    conf_mse = mse(conf_new_pred, y_test)
 
     # Store results
     results = {
         'pb_i_cov': pb_i_cov,
+        'conf_i_cov': conf_i_cov,
         'pb_ii_cov': pb_ii_cov,
+        'conf_ii_cov': conf_ii_cov,
         'pb_iii_cov': pb_iii_cov,
+        'conf_iii_cov': conf_iii_cov,
         'pb_iv_cov': pb_iv_cov,
+        'conf_iv_cov': conf_iv_cov,
         'OOB_quantile': oob_quantile,
+        'quantile': quantile,
         'pb_time': pb_time,
+        'conf_time': conf_time,
         'pb_mse': pb_mse,
+        'conf_mse': conf_mse
     }
     filename = os.path.join(results_dir, file[:-4] + '_results.npy')
     np.save(filename, results)
 
-file_list = list(filter(lambda file: file.endswith(f'block_{current_block}.pkl'), filter(lambda file: file.endswith('.pkl'), os.listdir(os.path.join(os.getcwd(), 'simulations_euc', 'pb_25/')))))
+file_list = list(filter(lambda file: file.endswith(f'block_{current_block}.pkl'), filter(lambda file: file.endswith('.pkl'), os.listdir(os.path.join(os.getcwd(), 'simulations_euc', 'data/')))))
 total_files = len(file_list)
 
 with tqdm_joblib(tqdm(desc="Percentage of tasks completed:", total = total_files)) as progress_bar:
