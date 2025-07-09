1c7b2ae79893e968cdc04d6dd75f6e69d327537b
Diego <Diego@MacBook-Pro-3.local>
2025-07-09 03:41:35 +0200

    job

---
+++ job_H2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+module load python/3.9.9
+lscpu
+python simulations_H2/H2_parallel.py $block
+git add .
+git commit -m "Simulations Hyperboloid block $block"
+git push
+
+# for b in {1..40}
+# do
+#   sbatch --time=01:35:00 -n 1 --cpus-per-task=32 --mem=18GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="%j-H2-block$b.out" job_H2.sh
+# done
+
+# sbatch --time=01:35:00 -n 1 --cpus-per-task=32 --mem=18GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=1 --output="%j-H2-block1.out" job_H2.sh
