ed6dc407e0fab394a0eeee56155e545cc1be5838
Diego <Diego@MacBook-Pro-3.local>
2025-08-13 03:51:02 +0200

    job SPD with uniform

---
+++ job_SPD_unif.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+module load cesga/2020 python/3.9.9
+export PYTHONNOUSERSITE=1
+source $STORE/pballs/bin/activate
+lscpu
+python simulations_SPD/ALL_main_parallel_unif.py $block
+git add .
+git commit -m "Simulations SPD block $block"
+git push
+
+# for b in {1..50}
+# do
+#   sbatch --time=03:55:00 -n 1 --cpus-per-task=32 --mem=16GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="%j-SPD-block$b.out" job_SPD_unif.sh
+# done
+
+# sbatch --time=03:55:00 -n 1 --cpus-per-task=32 --mem=16GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=100 --output="%j-SPD-block100.out" job_SPD_unif.sh
